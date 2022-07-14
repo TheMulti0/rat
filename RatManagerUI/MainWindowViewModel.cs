eaf604860cd20b445e338e347498228e5f6da592
TheMulti0 <multi@codeprecise.com>
2022-07-14 19:55:36 +0300

    Added WPF

---
+++ RatManagerUI/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PropertyChanged.SourceGenerator;
+
+namespace RatManagerUI
+{
+    internal partial class MainWindowViewModel
+    {
+        private readonly RatManagerService _service = new();
+
+        [Notify]
+        private int _clientCount;
+
+        public MainWindowViewModel()
+        {
+            _service.ClientCount.Subscribe(i => ClientCount = i);
+        }
+    }
+}
---
+++ RatManagerUI/RatManagerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Linq;
+using Server.Interop;
+
+namespace RatManagerUI
+{
+    internal class RatManagerService
+    {
+        public IObservable<int> ClientCount { get; }
+
+        public RatManagerService()
+        {
+            IRatManager manager = new RatManager(4545);
+
+            ClientCount = Observable
+                .Interval(TimeSpan.FromMilliseconds(100))
+                .Select(_ => manager.GetClientCount())
+                .DistinctUntilChanged();
+        }
+    }
+}
