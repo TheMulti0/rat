735efefbc28960329891614f39cc301769fe2417
TheMulti0 <multi@codeprecise.com>
2022-08-05 17:53:58 +0300

    Extracted Serializer's implementation to source file

---
+++ Communication/Serializer.cpp
@@ -0,0 +1,28 @@
+﻿#include "Serializer.h"
+
+Serializer::Serializer(const int size):
+	_index(0),
+	_size(size),
+	_data(new char[size])
+{
+}
+
+void Serializer::Add(SharedSpan item)
+{
+	Add(item.Data(), item.Size());
+}
+
+void Serializer::Add(const char* item, const int size)
+{
+	std::copy_n(item, size, _data.get() + _index);
+	_index += size;
+}
+
+SharedSpan Serializer::Data()
+{
+	auto span = SharedSpan(_size);
+
+	std::copy_n(_data.release(), _size, span.Data());
+
+	return span;
+}
--- Communication/Serializer.h
+++ Communication/Serializer.h
@@ -2,35 +2,18 @@
 
 #include <memory>
 
+#include "SharedSpan.h"
+
 class Serializer
 {
 public:
-	explicit Serializer(const int size) :
-		_index(0),
-		_size(size),
-		_data(new char[size])
-	{
-	}
+	explicit Serializer(int size);
 
-	void Add(SharedSpan item)
-	{
-		Add(item.Data(), item.Size());
-	}
+	void Add(SharedSpan item);
 
-	void Add(const char* item, const int size)
-	{
-		std::copy_n(item, size, _data.get() + _index);
-		_index += size;
-	}
+	void Add(const char* item, const int size);
 
-	SharedSpan Data()
-	{
-		auto span = SharedSpan(_size);
-
-		std::copy_n(_data.release(), _size, span.Data());
-
-		return span;
-	}
+	SharedSpan Data();
 
 private:
 	int _index;
